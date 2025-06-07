0855cd85209ebc85ab354da2b31933cd402962fd
onu032001 <85744292+onu032001@users.noreply.github.com>
2025-06-07 14:08:38 +0900

    added shortcut code

---
+++ shortcut-codes/patapim-shortcut.js
@@ -0,0 +1,21 @@
+let Y = function (f) {
+    return (function (x) {
+        return f(function (y) {
+            return x(x)(y);
+        });
+    })(function (x) {
+        return f(function (y) {
+            return x(x)(y);
+        });
+    });
+};
+let F = function (f) {
+    return function (x) {
+        if (x <= 0) {
+            return 'patapim';
+        } else {
+            return 'brr '+f(x-1);
+        }
+    };
+};
+completion(F(2));
