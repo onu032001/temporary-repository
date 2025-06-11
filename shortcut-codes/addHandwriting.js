e60bc97262a1c010bec595d8741e0e79ae2685e5
onu032001 <85744292+onu032001@users.noreply.github.com>
2025-06-11 17:04:30 +0900

    added

---
+++ shortcut-codes/addHandwriting.js
@@ -0,0 +1,8 @@
+function addHandwriting() {
+    let handwritingLink = document.createElement('link');
+    let handwritingStyle = document.createElement('style');
+    handwritingLink.setAttribute('rel', 'stylesheet');
+    handwritingLink.setAttribute('href', 'https://hangeul.pstatic.net/hangeul_static/css/NanumJarHaGoIssEo.css');
+    handwritingStyle.innerHTML = ':not(math, math *) {font-family: \'NanumJarHaGoIssEo\'';
+    document.head.append(handwritingLink, handwritingStyle);
+}
